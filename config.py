26c1888c825d06c43689dfbd56596addc4a0a16b
Artem <106912156+TroyaDevelop@users.noreply.github.com>
2024-12-22 15:16:34 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-token = "vk1.a.BT6y4L2iTs_3IMR9m5YPuYUPVFR_R6LHT_Vw89mff-jmEY37n4e2wCG8U9AXTe0GA-8R7bv7rc1mOSD0TR7dylaouU9wUc9V3qZBcIR60euacOGXaMKogCndLPv7oXN1SARmZD840NM4o3tEBIImLOOKyig0aGDQMMEzdP-3IJY4347rMju_KmfYQIppI7a5-GoIQtpZnum59WQxgPbJlg"
-group_id = "228165918"
-editable_stats = ["strength", "dexterity", "intelligence", "wisdom", "charisma", "constitution"]
